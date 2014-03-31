0c94ee5a5bfd343d727ba96088a8ec8475568767
Alex Perelman <alex@alexperelman.com>
2014-03-31 12:46:29 -0700

    Title and meta description helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title
+    base_title = 'Rails 4 Template App'
+    @page_title.nil? ? base_title : "#{@page_title} | #{base_title}"
+  end
+  
+  def description
+    base_description = ''
+    @page_description.nil? ? base_description : @page_description
+  end
 end
